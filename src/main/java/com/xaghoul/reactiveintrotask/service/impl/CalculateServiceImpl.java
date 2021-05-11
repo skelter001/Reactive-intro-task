14c06be2f1add391c10a92992bfa4a1ae32e5604
Gamzat <unichtojenmusor@gmail.com>
2021-05-11 10:18:33 +0300

    fix: add backpressure drop to service

--- src/main/java/com/xaghoul/reactiveintrotask/service/impl/CalculateServiceImpl.java
+++ src/main/java/com/xaghoul/reactiveintrotask/service/impl/CalculateServiceImpl.java
@@ -77,7 +77,7 @@ public class CalculateServiceImpl implements CalculationService {
                                                 calculation.getResult(),
                                                 calculation.getExecutionTime()
                                         )
-                                )
+                                ).onBackpressureDrop()
                 );
     }
 }
